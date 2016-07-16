c66a5f4676b3feee6fc594c304e812a951c060d9
oneWarcraft <wjw6710@163.com>
2016-07-16 21:31:40 +0800

    扫描二维码

--- QRCode/QRCode/classes/Scanning/ScanningController.swift
+++ QRCode/QRCode/classes/Scanning/ScanningController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ScanningController: UIViewController {
 //class ScanningController: UIViewController {
@@ -14,10 +15,17 @@ class ScanningController: UIViewController {
     @IBOutlet weak var scanLineBottomCons: NSLayoutConstraint!
     @IBOutlet weak var scanBgView: UIView!
     
+    var session : AVCaptureSession?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 1.开始扫描动画
         startScanAnimation()
+        
+        // 2.开始扫描
+        startScanning()
     }
     
 }
@@ -34,4 +42,33 @@ extension ScanningController {
             self.scanBgView.layoutIfNeeded()
         }
     }
+    
+    private func startScanning() {
+        // 1.创建输入(摄像头)
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        guard let input = try? AVCaptureDeviceInput(device: device) else {
+            print("获取摄像头过程中有异常")
+            return
+        }
+        
+        // 2.创建输出(Metadata)
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        
+        // 3. 创建会话，联系输入和输出
+        let session = AVCaptureSession()
+        session.addInput(input)
+        session.addOutput(output)
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        self.session = session
+
+    }
 }
+
+extension ScanningController: AVCaptureMetadataOutputObjectsDelegate {
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        print("扫描结果")
+    }
+}
+
+
